ed708a5f38017841db8458fdd71fcc9bf6fb38ca
Pär Eriksson <pe222gq@student.lnu.se>
2018-03-11 21:42:40 +0100

    Adding template engine.

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 import express from 'express'
+import router from './route'
 
 const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './template')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(8000, () => {
   console.log('Server running on http://localhost:8000/')
 })
+
+// Load application routes.
+app.use('/', router)
+
+// // Custom 404 page.
+// app.use((req, res) => {
+//   res.type('text/html')
+//   res.status(404)
+//   res.render('errors/404', {layout: 'error'})
+// })
+
+// // Custom 500 page.
+// app.use((err, req, res, next) => {
+//   //console.error(err.stack)
+//   res.type('text/html')
+//   res.status(500)
+//   res.render('errors/500', {layout: 'error'})
+//   next()
+// })
